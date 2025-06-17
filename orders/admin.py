5f4594d3a985733913017b8ed8c6740211bb73ff
rdralves <rdr.alves@gmail.com>
2025-06-17 15:59:30 -0300

    feature/admin_painel: add campos para pesquisa, add campos para evitar manipulação das datas manualmente.

--- orders/admin.py
+++ orders/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from .models import Order
 
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('tracking_code', 'sender', 'recipient',
-                    'status', 'created_at', 'updated_at')
+    list_display = (
+        'tracking_code', 'sender', 'recipient', 'status', 'weight_kg',
+        'declared_value', 'created_at', 'updated_at'
+    )
     search_fields = ('tracking_code', 'sender', 'recipient')
-    list_filter = ('status',)
+    list_filter = ('status', 'created_at')
+    readonly_fields = ('created_at', 'updated_at')
+    ordering = ('-created_at',)
